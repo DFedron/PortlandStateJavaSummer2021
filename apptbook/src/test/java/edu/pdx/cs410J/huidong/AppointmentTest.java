fdfa1ecc9d9b31181606b5e6677699bed666a69e
huidong <huidong@pdx.edu>
2021-07-16 00:18:22 -0700

    Test pass for origin test!

--- apptbook/src/test/java/edu/pdx/cs410J/huidong/AppointmentTest.java
+++ apptbook/src/test/java/edu/pdx/cs410J/huidong/AppointmentTest.java
@@ -20,6 +20,23 @@ public class AppointmentTest {
 
   @Test
   void getBeginTimeStringNeedsToBeImplemented() throws ParseException {
+//    String s = "03/03/2021 12:00";
+//    DateFormat simpleD = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+//    Date BeginTime = simpleD.parse(s);
+//    System.out.println("BeginTime: " + BeginTime);
+//    s = "04/04/2021 16:00";
+//    Date EndTime = simpleD.parse(s);
+//    Appointment appointment = new Appointment("Date with Lisa", BeginTime,EndTime);
+    Appointment appointment = new Appointment();
+    assertThrows(NullPointerException.class, appointment::getBeginTimeString);
+  }
+@Test
+void getEndTimeStringNeedsToBeImplemented() throws ParseException {
+    Appointment appointment = new Appointment();
+    assertThrows(NullPointerException.class, appointment::getEndTimeString);
+}
+  @Test
+  void initiallyAllAppointmentsHaveTheSameDescription() throws ParseException {
     String s = "03/03/2021 12:00";
     DateFormat simpleD = new SimpleDateFormat("MM/dd/yyyy HH:mm");
     Date BeginTime = simpleD.parse(s);
@@ -27,13 +44,7 @@ public class AppointmentTest {
     s = "04/04/2021 16:00";
     Date EndTime = simpleD.parse(s);
     Appointment appointment = new Appointment("Date with Lisa", BeginTime,EndTime);
-    assertThrows(UnsupportedOperationException.class, appointment::getBeginTimeString);
-  }
-
-  @Test
-  void initiallyAllAppointmentsHaveTheSameDescription() {
-    Appointment appointment = new Appointment();
-    assertThat(appointment.getDescription(), containsString("not implemented"));
+    assertThat(appointment.getDescription(), containsString("Date with Lisa"));
   }
 
   @Test
